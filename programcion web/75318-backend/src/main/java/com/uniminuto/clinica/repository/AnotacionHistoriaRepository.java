a6d64df9ba63242a3b56aed9235353ff9473cfcc
CEDULA-3 <gruendgru@gmail.com>
2025-10-19 16:19:38 -0500

    Limpiando repositorio

--- programcion web/75318-backend/src/main/java/com/uniminuto/clinica/repository/AnotacionHistoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uniminuto.clinica.repository;
-
-import com.uniminuto.clinica.entity.AnotacionHistoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnotacionHistoriaRepository extends JpaRepository<AnotacionHistoria, Long> {
-}
